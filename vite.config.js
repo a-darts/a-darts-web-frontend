05ad7b5c668a91de0174f5ba266cadd951f29545
arianaporroche <874055@unizar.es>
2026-06-06 16:25:55 +0200

    Update API_BASE_URL and SOCKET_URL to .env

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react()],
+
+    define: {
+      'process.env.API_BASE_URL': JSON.stringify(env.VITE_API_BASE_URL || 'http://localhost:3000/api'),
+      'process.env.SOCKET_URL': JSON.stringify(env.VITE_SOCKET_URL || 'http://localhost:3000'),
+    }
+  };
+});
